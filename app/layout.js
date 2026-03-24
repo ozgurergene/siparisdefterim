fb4f3dddbd0f7be857f667c61c0c79d09e21e06a
Duygu Fidan <ozgurergene@gmail.com>
2026-03-24 15:35:29 +0300

    Add layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+﻿export const metadata = {
+  title: "SiparişDefterim",
+  description: "Instagram siparişlerini yönet",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  )
+}
